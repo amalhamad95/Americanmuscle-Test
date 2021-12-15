4934f66a00ce0c1b8b29f1bb387d84e30a474a01
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2021-12-15 17:56:26 +0200

    Update commands.js
    add "saveCookies" custom command
    add "clearSiteData" custom command
    add "checkPageUrl" custom command
    add "checkCartCount" custom command
    import "real event" library

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,32 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import 'cypress-real-events/support'
+
+//Custom Command to Save Site Cookies
+Cypress.Commands.add('saveCookies', (count) => {
+    // Save Cookies to prevent Login everytime
+    Cypress.Cookies.defaults({
+        preserve: (cookie) => {
+            return true;
+        }
+    })
+})
+
+//Custom Command to Clear Site Data
+Cypress.Commands.add('clearSiteData', (count) => {
+    cy.clearCookies() // clear all cookies
+    cy.clearLocalStorage()
+})
+
+//Custom Command to Check Current Page URL and Title 
+Cypress.Commands.add('checkPageUrl', (url, title) => {
+    cy.log('Check page url')
+    cy.url().should('include', url)
+    cy.log('Check page title')
+    cy.get('h1[class$="header"]').contains(title)
+})
+
+
+//Custom Command to Check Top Nav Cart Count
+Cypress.Commands.add('checkCartCount', (count) => {
+    cy.get('.cart_trigger span.cart_count')
+        .should('have.text', count)
+})
